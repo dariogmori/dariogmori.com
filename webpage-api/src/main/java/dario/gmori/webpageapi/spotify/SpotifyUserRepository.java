3ea328bd03e71d2078bbef34fd7ab1bdfc25f3a8
Dario <darionuberu@gmail.com>
2024-06-03 19:15:13 +0200

    fix: updated table

--- webpage-api/src/main/java/dario/gmori/webpageapi/spotify/SpotifyUserRepository.java
+++ webpage-api/src/main/java/dario/gmori/webpageapi/spotify/SpotifyUserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SpotifyUserRepository extends JpaRepository<SpotifyUser, Long> {
-    @Query(value = "SELECT s FROM SpotifyUser s", nativeQuery = true)
+    @Query(value = "SELECT s FROM spotify_users s", nativeQuery = true)
     SpotifyUser findUserInfo();
 }
