e9650deb36c5ab0daf5223988ef567943b8e509e
Dario <darionuberu@gmail.com>
2024-06-03 15:42:53 +0200

    fix: changed to rest controller

--- webpage-api/src/main/java/dario/gmori/webpageapi/spotify/SpotifyController.java
+++ webpage-api/src/main/java/dario/gmori/webpageapi/spotify/SpotifyController.java
@@ -4,11 +4,11 @@ import dario.gmori.webpageapi.spotify.dto.SpotifyUserResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/spotify")
 @RequiredArgsConstructor
 public class SpotifyController {
