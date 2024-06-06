52809590da7cc0b16201a19a572b6cf166fd785d
Dario <darionuberu@gmail.com>
2024-06-06 20:38:02 +0200

    feat: http post method

--- webpage-api/src/main/java/dario/gmori/webpageapi/common/requests/spotify/HttpRestService.java
+++ webpage-api/src/main/java/dario/gmori/webpageapi/common/requests/spotify/HttpRestService.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 
@@ -13,7 +14,12 @@ public class HttpRestService {
     public JsonNode makeGetRequest(String uri, HttpHeaders headers){
         RestTemplate restTemplate = new RestTemplate();
         HttpEntity<String> entity = new HttpEntity<>(headers);
-
         return restTemplate.exchange(uri, HttpMethod.GET,entity,JsonNode.class).getBody();
+    }
+
+    public JsonNode makePostRequest(String uri, HttpHeaders headers, MultiValueMap<String, String> body) {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(body, headers);
+        return restTemplate.exchange(uri, HttpMethod.POST, entity, JsonNode.class).getBody();
     }
 }
