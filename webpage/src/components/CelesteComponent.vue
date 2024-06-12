a5d8eed1468382f79cab67108ea15874c88bf8b2
Dario G. Mori <darionuberu@gmail.com>
2024-06-12 17:57:06 +0200

    feat: readded celeste component

---
+++ webpage/src/components/CelesteComponent.vue
@@ -0,0 +1,150 @@
+<template>
+    <el-card class="card-celeste">
+        <el-row justify="space-between">
+            <el-col :span="8">
+                    <el-row justify="space-evenly">
+                        <el-column :span="6">
+                            <img src="../assets/celeste/icon_celeste.jpg" class="avatar"/>
+                            <el-row justify="center" align="middle">
+                                <el-col :span="12">
+                                    <h1 class="username">Toto</h1>
+                                </el-col>
+                                <el-col :span="10">
+                                    <el-row align="middle">
+                                        <el-col :span="14">
+                                            <h1 class="counter">x192</h1>
+                                        </el-col>
+                                        <el-col :span="8">
+                                            <img :src="strawberry" style="width:100% " />
+                                        </el-col>
+                                    </el-row>
+                                </el-col>
+                            </el-row>
+                        </el-column>
+                        <el-column :span="18" style="flex:auto">
+                        </el-column>
+                    </el-row>
+            </el-col>
+            <el-col :span="16" align="top">
+                <el-row justify="space-between">
+                    <el-col :span="16">
+                    </el-col>
+                    <el-col :span="8">
+                        <el-menu class="el-menu-demo" mode="horizontal" :ellipsis="false"
+                            :default-active="active.activeIndex" background-color=#ad914193
+                            active-text-color=#250505 text-color=#552d12; style="border-radius:10px; border-style: none; border-width: 2px; padding-left:10px; padding-right:10px; width:fit-content ">
+                            <el-row justify="end">
+                                <el-col :span="12">
+                                    <el-menu-item index="1" @click="active.activeIndex='1'">
+                                        <img :src="active.activeIndex === '1' ? heartActive : heartInactive" class="inside-button">
+                                    </el-menu-item>
+                                </el-col>
+                                <el-col :span="12">
+                                    <el-menu-item index="2" @click="active.activeIndex='2'">
+                                        <img :src="active.activeIndex === '2' ? goldenActive : goldenInactive" class="inside-button">
+                                    </el-menu-item>
+                                </el-col>
+                            </el-row>
+                        </el-menu>
+                    </el-col>
+                </el-row>
+                <el-card class="content-celeste">
+                    <el-row justify="center" v-if="active.activeIndex==='1'">
+                            <el-col :span="3" v-for="index in 8" :key="index">
+                                <!--<p class="counter">Ch{{index}}</p>-->
+                                <img :src="heartsAside[index-1]? heartBlue : dot" style="width:80%" class="heart"/>
+                                <img :src="heartsBside[index-1]? heartRed : dot" style="width:80%" class="heart"/>
+                                <img :src="heartsCside[index-1]? heartYellow : dot" style="width:80%" class="heart"/>
+                            </el-col>
+                    </el-row>
+                    <el-row justify="center" v-if="active.activeIndex==='2'">
+                            <el-col :span="3" v-for="index in 8" :key="index">
+                                <!--<p class="counter">Ch{{index}}</p>-->
+                                <img :src="goldenAside[index-1]? goldenIcon : dot" style="width:80%" class="heart"/>
+                                <img :src="goldenBside[index-1]? goldenIcon : dot" style="width:80%" class="heart"/>
+                                <img :src="goldenCside[index-1]? goldenIcon : dot" style="width:80%" class="heart"/>
+                            </el-col>
+                    </el-row>
+                </el-card>
+            </el-col>
+        </el-row>
+    </el-card>
+</template>
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+import goldenInactive from '@/assets/celeste/golden-strawberry.png'
+import goldenActive from '@/assets/celeste/golden-strawberry-active.png'
+import heartInactive from '../assets/celeste/heart-web.png'
+import heartActive from '../assets/celeste/heart-red-active.png'
+import strawberry from '../assets/celeste/strawberry.png'
+import heartRed from '../assets/celeste/heart-red.png'
+import heartYellow from '../assets/celeste/heart-yellow.png'
+import heartBlue from '../assets/celeste/heart-blue.png'
+import goldenIcon from '../assets/celeste/golden-strawberry-icon.png'
+import dot from '../assets/celeste/dot.png'
+const active = reactive({ activeIndex: ref('1') })
+
+const heartsAside = [
+  true,
+  true,
+  true,
+  true,
+  true,
+  true,
+  true,
+  true
+]
+const heartsBside = [
+  true,
+  true,
+  true,
+  true,
+  true,
+  true,
+  true,
+  true
+]
+const heartsCside = [
+  true,
+  true,
+  true,
+  true,
+  true,
+  true,
+  true,
+  true
+]
+const goldenAside = [
+  true,
+  true,
+  true,
+  true,
+  true,
+  true,
+  true,
+  false
+]
+const goldenBside = [
+  true,
+  true,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false
+]
+const goldenCside = [
+  true,
+  true,
+  true,
+  true,
+  true,
+  true,
+  true,
+  true
+]
+</script>
+<style scoped>
+@import '@/assets/styles/celeste.scss';
+</style>
--- webpage/src/views/CelesteView.vue
+++ webpage/src/views/CelesteView.vue
@@ -1,11 +1,46 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
+  <el-row>
+    <!-- LEFT CONTAINER -->
+    <el-col :span="6" class="left-container">
+      <img alt="celeste game" src="https://gaming-cdn.com/images/products/8003/orig/celeste-pc-mac-juego-steam-cover.jpg?v=1705489821" style="width: 90%">
+      <el-row>
+        <el-col :span="16">
+          <p>
+            {{ t('celeste.description') }}
+          </p>
+        </el-col>
+        <el-col :span="8">
+          <img alt="madeline" src="https://media.spelunky.fyi/mods/logo/01G8PXMPWZ2K3CVW70DJ5HEMGA/1658626661282822.gif" style="width: 90%">
+        </el-col>
+      </el-row>
+      <el-row>
+        </el-row>
+    </el-col>
 
+    <!-- CENTER CONTAINER -->
+    <el-col :span="12" class="center-container">
+      <h1>{{ t('celeste.title') }}</h1>
+    </el-col>
+
+    <!-- RIGHT CONTAINER -->
+    <el-col :span="6" class="right-container">
+      <el-col :span="24">
+        <CelesteComponent />
+      </el-col>
+      <el-row justify="end">
+        <el-col :span="20" >
+        </el-col>
+      </el-row>
+    </el-col>
+  </el-row>
 </template>
 
-<style scoped>
+<script setup lang="ts">
+import CelesteComponent from "@/components/CelesteComponent.vue";
+import { useTranslation } from "i18next-vue";
+const { t } = useTranslation();
+</script>
 
+<style scoped>
+@import '../assets/styles/view.css';
 </style>
