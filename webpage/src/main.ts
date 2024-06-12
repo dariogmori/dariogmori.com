55c444130fd1f99d36ae45471c8fe59fd8c21918
Dario G. Mori <darionuberu@gmail.com>
2024-06-12 11:29:15 +0200

    feat: changed init from vuecli to vite

--- webpage/src/main.ts
+++ webpage/src/main.ts
@@ -1,5 +1,16 @@
-import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
 import './assets/styles/theme.css'
-createApp(App).use(router).mount('#app')
+import i18next from 'i18next'
+import I18NextVue from 'i18next-vue'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(I18NextVue, { i18next })
+app.mount('#app')
+
--- webpage/src/router/index.ts
+++ webpage/src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import {createRouter, createWebHashHistory, RouteRecordRaw} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes: Array<RouteRecordRaw> = [
@@ -8,12 +8,12 @@ const routes: Array<RouteRecordRaw> = [
     component: HomeView
   },
   {
-    path: '/about',
+    path: '/about-me',
     name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutMe.vue')
   }
 ]
 
