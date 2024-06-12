e23c76b282d64dc1b7edf49191433aa9b588ca86
Dario G. Mori <darionuberu@gmail.com>
2024-06-12 17:46:59 +0200

    feat: updated view css

--- webpage/src/views/AboutMe.vue
+++ webpage/src/views/AboutMe.vue
@@ -1,11 +1,48 @@
 <template>
-  <div class="main-container">
-    <img alt="Vue logo" src="/img/me-1.jpg" style="width: 30vh">
-    <h1>{{ t('about-me.title') }}</h1>
-    <p>
-      {{ t('about-me.description') }}
-    </p>
-  </div>
+  <el-row >
+    <el-col :span="6" class="left-container">
+      <img alt="Vue logo" src="/img/me-1.jpg" style="width: 90%">
+      <el-row>
+        <el-col :span="18">
+          <p>
+            {{ t('about-me.description') }}
+          </p>
+        </el-col>
+
+        <el-col :span="4">
+
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="4">
+        </el-col>
+
+        <el-col :span="18">
+          <p>
+            {{ t('about-me.description') }}
+          </p>
+        </el-col>
+      </el-row>
+    </el-col>
+    <el-col :span="12" class="center-container">
+      <h1>{{ t('about-me.title') }}</h1>
+    </el-col>
+    <el-col :span="6" class="right-container">
+      <el-col :span="20">
+        <img alt="asturies" src="https://centrodesarrollodirectivo.es/wp-content/uploads/2019/05/asturias-mapa-1024x619.png" style="width: 90%">
+      </el-col>
+      <el-row justify="end">
+        <el-col :span="20" >
+          <img alt="enby flag" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Nonbinary_flag.svg/640px-Nonbinary_flag.svg.png" style="width: 90%">
+        </el-col>
+      </el-row>
+      <el-col :span="20">
+        <img alt="me again" src="/img/me-2.jpg" style="width: 90%">
+      </el-col>
+
+
+    </el-col>
+  </el-row>
 </template>
 
 <style scoped>
