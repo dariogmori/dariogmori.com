0eba0f1d78232515c72053beaa383a10ce7b9511
Dario G. Mori <darionuberu@gmail.com>
2024-06-12 20:46:31 +0200

    feat: hover over images

--- webpage/src/views/AboutMe.vue
+++ webpage/src/views/AboutMe.vue
@@ -1,7 +1,7 @@
 <template>
   <el-row >
     <el-col :span="6" class="left-container">
-      <img alt="Vue logo" src="/img/me-1.jpg" style="width: 90%">
+      <img alt="Me" src="/img/me-1.jpg" class="image-display" style="width: 90%">
       <el-row>
         <el-col :span="18">
           <p>
@@ -23,22 +23,22 @@
           </p>
         </el-col>
       </el-row>
-      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Flag_of_Guatemala.svg/2560px-Flag_of_Guatemala.svg.png" style="width: 90%">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Flag_of_Guatemala.svg/2560px-Flag_of_Guatemala.svg.png" class="image-display" style="width: 90%">
     </el-col>
     <el-col :span="12" class="center-container">
       <h1>{{ t('about-me.title') }}</h1>
     </el-col>
     <el-col :span="6" class="right-container">
       <el-col :span="20">
-        <img alt="asturies" src="https://centrodesarrollodirectivo.es/wp-content/uploads/2019/05/asturias-mapa-1024x619.png" style="width: 90%">
+        <img alt="asturies" src="https://centrodesarrollodirectivo.es/wp-content/uploads/2019/05/asturias-mapa-1024x619.png" class="image-display" style="width: 90%">
       </el-col>
       <el-row justify="end">
         <el-col :span="20" >
-          <img alt="enby flag" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Nonbinary_flag.svg/640px-Nonbinary_flag.svg.png" style="width: 90%">
+          <img alt="enby flag" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Nonbinary_flag.svg/640px-Nonbinary_flag.svg.png" class="image-display" style="width: 90%">
         </el-col>
       </el-row>
       <el-col :span="20">
-        <img alt="me again" src="/img/me-2.jpg" style="width: 90%">
+        <img alt="me again" src="/img/me-2.jpg" class="image-display" style="width: 90%">
       </el-col>
 
 
--- webpage/src/views/CelesteView.vue
+++ webpage/src/views/CelesteView.vue
@@ -2,7 +2,7 @@
   <el-row>
     <!-- LEFT CONTAINER -->
     <el-col :span="6" class="left-container">
-      <img alt="celeste game" src="https://gaming-cdn.com/images/products/8003/orig/celeste-pc-mac-juego-steam-cover.jpg?v=1705489821" style="width: 90%">
+      <img alt="celeste game" src="https://gaming-cdn.com/images/products/8003/orig/celeste-pc-mac-juego-steam-cover.jpg?v=1705489821" class="image-display" style="width: 90%">
       <el-row>
         <el-col :span="16">
           <p>
@@ -10,7 +10,7 @@
           </p>
         </el-col>
         <el-col :span="8">
-          <img alt="madeline" src="https://media.spelunky.fyi/mods/logo/01G8PXMPWZ2K3CVW70DJ5HEMGA/1658626661282822.gif" style="width: 90%">
+          <img alt="madeline" src="https://media.spelunky.fyi/mods/logo/01G8PXMPWZ2K3CVW70DJ5HEMGA/1658626661282822.gif" class="image-display" style="width: 90%">
         </el-col>
       </el-row>
     </el-col>
@@ -26,7 +26,7 @@
         <CelesteComponent />
       </el-col>
       <el-row>
-        <VideoCardComponent :video="'https://www.youtube.com/embed/_iBkkwy0fcM'" :title="'Cool speedy thingie'" :article="'yay me go fast'"></VideoCardComponent>
+        <VideoCardComponent :video="'https://www.youtube.com/embed/_iBkkwy0fcM'" :title="'Cool speedy thingie'" :article="'yay me go fast'" ></VideoCardComponent>
       </el-row>
     </el-col>
   </el-row>
