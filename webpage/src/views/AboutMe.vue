877abb01d4864b6c5adeedae0991972bd0fef3d2
Dario G. Mori <darionuberu@gmail.com>
2024-06-12 16:53:01 +0200

    feat: moved css for view into different file

--- webpage/src/views/AboutMe.vue
+++ webpage/src/views/AboutMe.vue
@@ -1,29 +1,16 @@
 <template>
   <div class="main-container">
     <img alt="Vue logo" src="/img/me-1.jpg" style="width: 30vh">
-    <h1>{{ $t('about-me.title') }}</h1>
+    <h1>{{ t('about-me.title') }}</h1>
     <p>
+      {{ t('about-me.description') }}
     </p>
   </div>
 </template>
 
 <style scoped>
 @import '../assets/styles/palette.css';
-.main-container{
-  opacity: 0;
-  transform: translateY(-50px);
-  animation: fadeFall 0.5s ease-out forwards;
-}
-@keyframes fadeFall {
-  0% {
-    opacity: 0;
-    transform: translateY(-100px);
-  }
-  100% {
-    opacity: 1;
-    transform: translateY(0);
-  }
-}
+@import '../assets/styles/view.css';
 </style>
 
 <script setup lang="ts">
