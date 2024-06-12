3ebe746d90cab3343e3ada692925ae4b1db8fa33
Dario G. Mori <darionuberu@gmail.com>
2024-06-12 16:55:24 +0200

    feat: added element-plus

--- webpage/src/main.ts
+++ webpage/src/main.ts
@@ -4,9 +4,15 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import './assets/styles/theme.css'
+
+// i18n
 import i18next from 'i18next'
 import I18NextVue from 'i18next-vue'
 import Backend from 'i18next-http-backend'
+
+//element plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 
@@ -24,6 +30,7 @@ i18next
     });
 
 app.use(createPinia())
+app.use(ElementPlus)
 app.use(router)
 app.use(I18NextVue, { i18next })
 app.mount('#app')
