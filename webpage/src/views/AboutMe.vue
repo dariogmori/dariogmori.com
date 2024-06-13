fe0121ad5117f4391b66591df0419a844992c126
Dario G. Mori <darionuberu@gmail.com>
2024-06-13 20:46:53 +0200

    feat: about me logo

--- webpage/src/views/AboutMe.vue
+++ webpage/src/views/AboutMe.vue
@@ -54,8 +54,8 @@
     </el-col>
 
     <!-- CENTER CONTAINER -->
-    <el-col :span="12" class="center-container">
-      <h1>{{ t('about-me.title') }}</h1>
+    <el-col :span="12" class="center-container" >
+      <el-image :src="getLogoURI()" class="logo" />
     </el-col>
 
 
@@ -77,5 +77,27 @@
 
 <script setup lang="ts">
 import { useTranslation } from "i18next-vue";
+import { onMounted, reactive } from "vue";
+
 const { t } = useTranslation();
+const config = reactive({
+  theme: 'light',
+  lang: 'es'
+})
+onMounted(() => {
+  const savedTheme = localStorage.getItem('theme')
+  const savedLang = localStorage.getItem('lang')
+  if (savedTheme) {
+    config.theme = savedTheme
+    document.documentElement.setAttribute('data-theme', config.theme)
+  }
+  if( savedLang ) {
+    config.lang = savedLang
+    document.documentElement.setAttribute('data-lang', config.lang)
+  }
+})
+
+function getLogoURI() {
+  return '/img/about-me/logo-' + config.theme + '-' + config.lang + '.png'
+}
 </script>
