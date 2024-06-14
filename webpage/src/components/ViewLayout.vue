77fff1032922f2fb7c43f086124823799e4402a5
Dario G. Mori <darionuberu@gmail.com>
2024-06-14 15:41:17 +0200

    feat: layout container

---
+++ webpage/src/components/ViewLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-col :span="getSpan(0)" class="left-container" >
+    <slot name="left-col"></slot>
+  </el-col>
+
+  <el-col :span="getSpan(1)" class="center-container" >
+    <slot name="center-col"></slot>
+  </el-col>
+
+  <el-col :span="getSpan(2)" class="right-container">
+    <slot name="right-col"></slot>
+  </el-col>
+</template>
+
+<script setup lang="ts">
+import { getSpan } from "@/scripts/layout";
+</script>
+
+
+<style scoped>
+@import '../assets/styles/palette.css';
+@import '../assets/styles/view.css';
+</style>
