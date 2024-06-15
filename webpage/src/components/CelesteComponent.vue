e32df18bb98523d48220e6354f87a5b6bd8c83e9
Dario G. Mori <darionuberu@gmail.com>
2024-06-15 14:35:42 +0200

    feat: new hearts and strawberry

--- webpage/src/components/CelesteComponent.vue
+++ webpage/src/components/CelesteComponent.vue
@@ -3,7 +3,7 @@
         <el-row justify="space-between">
             <el-col :span="8">
                     <el-row justify="space-evenly">
-                        <el-column :span="6">
+                        <el-col >
                             <img src="../assets/celeste/icon_celeste.jpg" class="avatar"/>
                             <el-row justify="center" align="middle">
                                 <el-col :span="12">
@@ -15,14 +15,12 @@
                                             <h1 class="counter">x192</h1>
                                         </el-col>
                                         <el-col :span="8">
-                                            <img :src="strawberry" style="width:100% " />
+                                            <img :src="strawberry" class="pixel-art" style="width:100%;" />
                                         </el-col>
                                     </el-row>
                                 </el-col>
                             </el-row>
-                        </el-column>
-                        <el-column :span="18" style="flex:auto">
-                        </el-column>
+                        </el-col>
                     </el-row>
             </el-col>
             <el-col :span="16" align="top">
@@ -36,12 +34,12 @@
                             <el-row justify="end">
                                 <el-col :span="12">
                                     <el-menu-item index="1" @click="active.activeIndex='1'">
-                                        <img :src="active.activeIndex === '1' ? heartActive : heartInactive" class="inside-button">
+                                        <img :src="active.activeIndex === '1' ? heartActive : heartInactive" class="inside-button ">
                                     </el-menu-item>
                                 </el-col>
                                 <el-col :span="12">
                                     <el-menu-item index="2" @click="active.activeIndex='2'">
-                                        <img :src="active.activeIndex === '2' ? goldenActive : goldenInactive" class="inside-button">
+                                        <img :src="active.activeIndex === '2' ? goldenActive : goldenInactive" class="inside-button pixel-art">
                                     </el-menu-item>
                                 </el-col>
                             </el-row>
@@ -52,9 +50,9 @@
                     <el-row justify="center" v-if="active.activeIndex==='1'">
                             <el-col :span="3" v-for="index in 8" :key="index">
                                 <!--<p class="counter">Ch{{index}}</p>-->
-                                <img :src="heartsAside[index-1]? heartBlue : dot" style="width:80%" class="heart"/>
-                                <img :src="heartsBside[index-1]? heartRed : dot" style="width:80%" class="heart"/>
-                                <img :src="heartsCside[index-1]? heartYellow : dot" style="width:80%" class="heart"/>
+                                <img :src="heartsAside[index-1]? heartBlue : dot" style="width:100%" class="heart pixel-art"/>
+                                <img :src="heartsBside[index-1]? heartRed : dot" style="width:100%" class="heart pixel-art"/>
+                                <img :src="heartsCside[index-1]? heartYellow : dot" style="width:100%" class="heart pixel-art"/>
                             </el-col>
                     </el-row>
                     <el-row justify="center" v-if="active.activeIndex==='2'">
@@ -147,4 +145,8 @@ const goldenCside = [
 </script>
 <style scoped>
 @import '@/assets/styles/celeste.scss';
+
+.pixel-art{
+  image-rendering: crisp-edges;
+}
 </style>
