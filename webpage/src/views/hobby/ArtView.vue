cc38faabb60e4d38b3e9c07a8e69d52cb4b6bf4e
Dario <darionuberu@gmail.com>
2024-08-20 17:32:53 +0200

    feat: 2022 and helltaker

--- webpage/src/views/hobby/ArtView.vue
+++ webpage/src/views/hobby/ArtView.vue
@@ -4,7 +4,7 @@
     <template #left-col>
       <ArtComponent id="ace-attorney"  :images="['maya.png','gumshoe.png','edgeworth.png']" :month="'august'" :year="'2024'"/>
       <ArtComponent id="pfp2024"  :images="['2.png','3.png','4.png','1.png','5.png','6.png']" :month="'march'" :year="'2024'"/>
-      <ArtComponent id="helltaker" :images="['1.jpeg']" :month="'may'" :year="'2021'"/>
+      <ArtComponent id="pfp2022"  :images="['1.png']" :month="'june'" :year="'2022'"/>
     </template>
 
     <!-- CENTER CONTAINER -->
@@ -16,6 +16,7 @@
     <template #right-col>
       <ArtComponent id="me-sketches"  :images="['1.png','2.png','3.png','4.png']" :month="'august'" :year="'2024'"/>
       <ArtComponent id="pfp2023"  :images="['1.png','2.gif']" :month="'june'" :year="'2023'"/>
+      <ArtComponent id="helltaker" :images="['1.jpeg']" :month="'may'" :year="'2021'"/>
     </template>
 
   </ViewLayout>
