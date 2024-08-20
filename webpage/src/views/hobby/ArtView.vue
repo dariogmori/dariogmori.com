f9aa7f82626ec331df666e5f1efa250d80e0a4ae
Dario <darionuberu@gmail.com>
2024-08-20 18:22:29 +0200

    feat: snails house art

--- webpage/src/views/hobby/ArtView.vue
+++ webpage/src/views/hobby/ArtView.vue
@@ -30,10 +30,11 @@ const art = [
   {id: "me-sketches", month: "august", year: "2024", images: ['1.png','2.png','3.png','4.png']},
   {id: "mix/jose-emilio", month: "april", year: "2024", images: ['kiwi.gif']},
   {id: "pfp/2024", month: "march", year: "2024", images: ['2.png','3.png','4.png','1.png','5.png','6.png']},
+  {id: "tf2/merasmus", month: "october", year: "2023", images: ['merasmus.png','explanation.png']},
   {id: "pfp/2023", month: "june", year: "2023", images: ['1.png','2.gif']},
+  {id: "mix/snails-house", month: "january", year: "2023", images: ['1.jpg']},
   {id: "pfp/2022", month: "october", year: "2022", images: ['2.png','3.png']},
   {id: "pfp/2022", month: "june", year: "2022", images: ['1.png']},
-  {id: "tf2/merasmus", month: "october", year: "2023", images: ['merasmus.png','explanation.png']},
   {id: "helltaker", month: "may", year: "2021", images: ['1.jpeg'], vertical: true}
 ]
 </script>
