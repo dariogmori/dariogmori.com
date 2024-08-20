0c7ad0d3199a4d84a128f29573ce68ae36295c0d
Dario <darionuberu@gmail.com>
2024-08-20 19:01:09 +0200

    fix: fixed some missing thing

--- webpage/src/views/hobby/ArtView.vue
+++ webpage/src/views/hobby/ArtView.vue
@@ -26,18 +26,18 @@ import ArtComponent from "@/components/art/ArtComponent.vue";
 const { t } = useTranslation();
 
 const art = [
-  {id: "ace-attorney", month: "august", year: "2024", images: ['maya.png','gumshoe.png','edgeworth.png']},
-  {id: "me-sketches", month: "august", year: "2024", images: ['1.png','2.png','3.png','4.png']},
-  {id: "mix/jose-emilio", month: "april", year: "2024", images: ['kiwi.gif']},
-  {id: "pfp/2024", month: "march", year: "2024", images: ['2.png','3.png','4.png','1.png','5.png','6.png']},
-  {id: "class", month: "december", year: "2023", images: ['1.jpg','2.png','3.jpg','4.jpg','5.jpg']},
-  {id: "tf2/merasmus", month: "october", year: "2023", images: ['merasmus.png','explanation.png']},
-  {id: "pfp/2023", month: "june", year: "2023", images: ['1.png','2.gif']},
-  {id: "mix/snails-house", month: "january", year: "2023", images: ['1.jpg']},
-  {id: "pfp/2022", month: "october", year: "2022", images: ['2.png','3.png']},
-  {id: "mix/omori", month: "october", year: "2022", images: ['1.jpg']},
-  {id: "mix/oneshot", month: "september", year: "2022", images: ['1.jpg']},
-  {id: "pfp/2022", month: "june", year: "2022", images: ['1.png']},
+  {id: "ace-attorney", month: "august", year: "2024", images: ['maya.png','gumshoe.png','edgeworth.png'], vertical: false},
+  {id: "me-sketches", month: "august", year: "2024", images: ['1.png','2.png','3.png','4.png'], vertical: false},
+  {id: "mix/jose-emilio", month: "april", year: "2024", images: ['kiwi.gif'], vertical: false},
+  {id: "pfp/2024", month: "march", year: "2024", images: ['2.png','3.png','4.png','1.png','5.png','6.png'], vertical: false},
+  {id: "class", month: "december", year: "2023", images: ['1.jpg','2.png','3.jpg','4.jpg','5.jpg'], vertical: false},
+  {id: "tf2/merasmus", month: "october", year: "2023", images: ['merasmus.png','explanation.png'], vertical: false},
+  {id: "pfp/2023", month: "june", year: "2023", images: ['1.png','2.gif'], vertical: false},
+  {id: "mix/snails-house", month: "january", year: "2023", images: ['1.jpg'], vertical: false},
+  {id: "pfp/2022", month: "october", year: "2022", images: ['2.png','3.png'], vertical: false},
+  {id: "mix/omori", month: "october", year: "2022", images: ['1.jpg'], vertical: false},
+  {id: "mix/oneshot", month: "september", year: "2022", images: ['1.jpg'], vertical: false},
+  {id: "pfp/2022", month: "june", year: "2022", images: ['1.png'], vertical: false},
   {id: "helltaker", month: "may", year: "2021", images: ['1.jpeg'], vertical: true}
 ]
 </script>
