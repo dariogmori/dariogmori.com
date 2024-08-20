9a440841c138d0fefe7b0055c1172485c134f976
Dario <darionuberu@gmail.com>
2024-08-20 17:41:05 +0200

    feat: vertical image style

--- webpage/src/components/art/ArtComponent.vue
+++ webpage/src/components/art/ArtComponent.vue
@@ -3,7 +3,7 @@
     <h1>{{ t('month.'+ props.month) + ' ' + props.year }}</h1>
     <el-carousel :interval="4000" type="card" height="33vh" style="width:100%;">
       <el-carousel-item v-for="image_name in props.images" >
-        <img :src="'../../img/hobby/art/' + props.id + '/' + image_name" class="image-display" style="width: 100%">
+        <img :src="'../../img/hobby/art/' + props.id + '/' + image_name" class="image-display" :class="{ 'square-image' : !props.vertical, 'vertical-image' : props.vertical }" >
       </el-carousel-item>
     </el-carousel>
     <p>
@@ -15,17 +15,24 @@
 <script setup lang="ts">
 import { useTranslation } from "i18next-vue";
 import LayoutRow from "@/components/layout/LayoutRow.vue";
+import {isMobile} from "@/scripts/utils";
 
 const { t } = useTranslation();
 const props = defineProps({
   images: Array<String>,
   id: String,
   month: String,
-  year: String
+  year: String,
+  vertical: Boolean
 
 })
 </script>
 
 <style scoped>
-
+.square-image {
+  width: 100%;
+}
+.vertical-image{
+  max-height: 100%;
+}
 </style>
