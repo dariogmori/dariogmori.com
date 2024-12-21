c4e798819ed055a50e87bffa28415420710f56b1
Dario <darionuberu@gmail.com>
2024-12-22 00:01:05 +0100

    style: added padding at bottom

--- webpage/src/App.vue
+++ webpage/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <body :class="{ 'body-mobile' : isMobile(), 'body-desktop' : !isMobile() }">
-    <div style="z-index: 2;">
+    <div style="z-index: 2; margin-bottom: 10vh" >
       <router-view/>
     </div>
     <!--<iframe  v-if="!isMobile()" src="../game/webpage.html" style=" z-index: 1; position: fixed; bottom:10vh; left:35%; width: 30%; height:50%; border: 0; "></iframe>-->
