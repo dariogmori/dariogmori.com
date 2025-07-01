ccc0efe7aeb7c9cbcb313027b8f22fdf8b6d916c
Dario <darionuberu@gmail.com>
2025-07-01 19:06:08 +0200

    feat: created ad

--- webpage/src/views/talks/SustainablePlatformEngineering.vue
+++ webpage/src/views/talks/SustainablePlatformEngineering.vue
@@ -1,41 +1,61 @@
 <template>
-    <ViewLayout>
-        <template #left-col>
-            <LayoutRow :justify="'end'">
-                <el-carousel indicator-position="outside" :interval="4000" height="33vh" style="width:100%;">
-                    <el-carousel-item>
-                        <img :src="'../../img/projects/talks/sustainability/talk-title.jpg'" class="image-display" style="width: 100%">
-                    </el-carousel-item>
-                    <el-carousel-item>
-                        <img :src="'../../img/projects/talks/sustainability/guadalajara-1.jpg'" class="image-display" style="width: 50%">
-                    </el-carousel-item>
-                    <el-carousel-item>
-                        <img :src="'../../img/projects/talks/sustainability/talk-1.jpg'" class="image-display" style="width: 100%">
-                    </el-carousel-item>
-                </el-carousel>
-            </LayoutRow>
+    <div>
+        <ViewLayout>
+            <template #left-col>
+                <LayoutRow :justify="'end'">
+                    <el-carousel indicator-position="outside" :interval="4000" height="33vh" style="width:100%;">
+                        <el-carousel-item>
+                            <img :src="'../../img/projects/talks/sustainability/talk-title.jpg'" class="image-display" style="width: 100%">
+                        </el-carousel-item>
+                        <el-carousel-item>
+                            <img :src="'../../img/projects/talks/sustainability/guadalajara-1.jpg'" class="image-display" style="width: 50%">
+                        </el-carousel-item>
+                        <el-carousel-item>
+                            <img :src="'../../img/projects/talks/sustainability/talk-1.jpg'" class="image-display" style="width: 100%">
+                        </el-carousel-item>
+                    </el-carousel>
+                </LayoutRow>
+                
+            </template>
+
+            <template #center-col>
+                <img alt="Main Presentation" src="/img/projects/talks/sustainability/logo-kcd.jpg" class="image-display" style="width: 120px; ">
+                <h1> Sustainable Platform Engineering? </h1>
+                <h3>Having a green thumb without shooting up the costs</h3>
+                </template>
+
+            <template #right-col>
+                
+                <LayoutRow :justify="'end'">
+                    <iframe width="100%" height="355" src="https://www.youtube.com/embed/lTLttAcs1zE?si=EdE_M3TAI53or9WX" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    <a href="https://www.slideshare.net/slideshow/sustainable-platform-engineering-having-a-green-thumb-without-shooting-up-the-costs-dario-gutierrez-mori/277450234"> Slides  </a>
+                </LayoutRow>
             
-        </template>
-
-        <template #center-col>
-            <h1> Sustainable Platform Engineering? </h1>
-            <h3>Having a green thumb without shooting up the costs</h3>
-        </template>
-
-        <template #right-col>
-            <LayoutRow :justify="'end'">
-                <iframe width="100%" height="355" src="https://www.youtube.com/embed/lTLttAcs1zE?si=EdE_M3TAI53or9WX" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                <a href="https://www.slideshare.net/slideshow/sustainable-platform-engineering-having-a-green-thumb-without-shooting-up-the-costs-dario-gutierrez-mori/277450234"> Slides  </a>
-            </LayoutRow>
-           
-        </template>
-    </ViewLayout>
+            </template>
+        </ViewLayout>
+        <LayoutRow :justify="'end'">
+            <div class="fake-ad" v-if="ad.show" >
+                <div style="justify-content: space-between; align-content: end; display:flex;">
+                    <h3 class="cool-3d-text">
+                        Watch now! </h3>
+                    <el-button class="cool-3d-text" @click="ad.show = false" style="border: 3px solid var(--main-color);">x</el-button>
+                </div>
+                <a href="https://www.youtube.com/watch?v=lTLttAcs1zE" target="_blank" rel="noopener noreferrer">
+                    <img alt="Main Presentation" src="/img/projects/talks/sustainability/promo-1.jpg" style="width: 100%; cursor: pointer;border: 3px solid var(--background-color);"
+                    />
+                </a>
+            </div>
+            
+        </LayoutRow>
+</div>
+    
   </template>
   
   <script setup lang="ts">
   import ViewLayout from "@/components/layout/ViewLayout.vue";
   import LayoutRow from "@/components/layout/LayoutRow.vue";
   import { useTranslation } from "i18next-vue";
+import { reactive } from "vue";
   const game = {
     id : 'flerovium',
     date: '20-10-2023',
@@ -43,12 +63,42 @@
   }
   const { t } = useTranslation();
   
+  const ad = reactive({
+    show: true
+  });
+
   function openGameLink() {
     window.open('https://dariogmori.itch.io/the-half-life-of-flevorium', '_blank')
   }
+
+    const images = [
+    'talk-title.jpg',
+    'guadalajara-1.jpg',
+    'talk-1.jpg'
+    ]
   </script>
   
   <style scoped>
   @import '../../assets/styles/palette.css';
   @import '../../assets/styles/view.css';
+  .fake-ad{
+    width: 20%;
+    position:fixed;
+    bottom: 30px;
+    right: 60px; 
+    animation: ad 1s linear infinite;
+  }
+  @keyframes ad {
+    0% { width: 20%; }
+    50% { width: 21%; }
+    100% { width: 20%; }
+  }
+  .cool-3d-text {
+    color: var(--main-color);
+    text-shadow:
+        -2px -2px 0 var(--background-color),
+        1px -1px 0 var(--background-color),
+        -1px  1px 0 var(--background-color),
+        1px  1px 0 var(--background-color);           
+  }
   </style>
