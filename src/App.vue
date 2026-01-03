b3988e5ed37b78a1a992232b685296d915546842
Dario G. Mori <dario@resiz.es>
2026-01-03 17:43:01 +0100

    fix: status

--- src/App.vue
+++ src/App.vue
@@ -25,46 +25,46 @@
       </div>
     </div> 
     <el-row justify="center" style="z-index: 11; margin-bottom: 10vh" >
-            <el-menu 
-                mode="horizontal"
-                :class="{ 'mobile': isMobile(), 'desktop': !isMobile(),'el-menu' : true}"
-                style="position:fixed; bottom:0vh; border: 4px solid var(--secondary-color);"
-                :ellipsis="true"
-            >
-              <el-menu-item index="1" @click="goToRoute('/')"><el-icon><HomeFilled /></el-icon>{{ isMobile() ? '': 'Home' }}</el-menu-item>
-              <el-menu-item index="2" @click="goToRoute('/about-me')"><el-icon><UserFilled /></el-icon>{{ isMobile() ? '': t('about-me.title') }}</el-menu-item>
-              <el-sub-menu index="3">
-                <template #title><el-icon><Management /></el-icon>{{ isMobile() ? '': t('projects.title') }}</template>
-                <el-sub-menu index="3-1">
-                  <template #title>{{ t('games.title') }}</template>
-                  <el-menu-item index="3-1-1" @click="goToRoute('/games/big-crunch')">Big-Crunch</el-menu-item>
-                  <el-menu-item index="3-1-2" @click="goToRoute('/games/how-do-i-even-win-this-game')">HDIEWTG?</el-menu-item>
-                  <el-menu-item index="3-1-3" @click="goToRoute('/games/a-nice-place-4-art')">A nice play 4 art</el-menu-item>
-                  <el-menu-item index="3-1-4" @click="goToRoute('/games/the-half-life-of-flerovium')">The half life of flerovium</el-menu-item>
-                  <el-menu-item index="3-1-5" @click="goToRoute('/games/medium-expendedore')">Medium Expendedore</el-menu-item>
-                  <el-menu-item index="3-1-6" @click="goToRoute('/games/kiwiq')">Kiwiq</el-menu-item>
-                </el-sub-menu>
-                <el-sub-menu index="3-2">
-                  <template #title>{{ t('projects.talks.title') }}</template>
-                  <el-menu-item index="3-2-1" @click="goToRoute('/talks/sustainable-platform-engineering')">Sustainable Platform Engineering?</el-menu-item>
-                </el-sub-menu>
-                <el-menu-item index="3-3" @click="goToRoute('/blog')">Blog</el-menu-item>
-              </el-sub-menu>
-              <el-sub-menu index="4">
-                <template #title><el-icon><StarFilled /></el-icon>{{ isMobile() ? '': 'Hobbies' }}</template>
-                <el-menu-item index="4-1" @click="goToRoute('/celeste')">{{ t('celeste.title') }}</el-menu-item>
-                <el-menu-item index="4-2" @click="goToRoute('/minecraft')">Minecraft </el-menu-item>
-                <el-menu-item index="4-3" @click="goToRoute('/art')">{{ t('art.title') }}</el-menu-item>
-              </el-sub-menu>
-              <el-sub-menu index="5">
-                <template #title><el-icon><Tools /></el-icon></template>
-                <!-- <el-menu-item index="5-1" @click="setTheme('light')">{{ t('config.theme.light-mode') }}</el-menu-item>
-                <el-menu-item index="5-1" @click="setTheme('dark')">{{ t('config.theme.dark-mode') }}</el-menu-item>
-                <el-menu-item index="5-1" @click="setTheme('dark-typical')">{{ 'Dark Typical' }}</el-menu-item> -->
-                <el-menu-item index="5-2" @click="changeLanguage('es')">{{ t('config.language.spanish') }}</el-menu-item>
-                <el-menu-item index="5-3" @click="changeLanguage('en')">{{ t('config.language.english') }}</el-menu-item>
-              </el-sub-menu>
-            </el-menu>
+      <el-menu 
+          mode="horizontal"
+          :class="{ 'mobile': isMobile(), 'desktop': !isMobile(),'el-menu' : true}"
+          style="position:fixed; bottom:0vh; border: 4px solid var(--secondary-color);"
+          :ellipsis="true"
+      >
+        <el-menu-item index="1" @click="goToRoute('/')"><el-icon><HomeFilled /></el-icon>{{ isMobile() ? '': 'Home' }}</el-menu-item>
+        <el-menu-item index="2" @click="goToRoute('/about-me')"><el-icon><UserFilled /></el-icon>{{ isMobile() ? '': t('about-me.title') }}</el-menu-item>
+        <el-sub-menu index="3">
+          <template #title><el-icon><Management /></el-icon>{{ isMobile() ? '': t('projects.title') }}</template>
+          <el-sub-menu index="3-1">
+            <template #title>{{ t('games.title') }}</template>
+            <el-menu-item index="3-1-1" @click="goToRoute('/games/big-crunch')">Big-Crunch</el-menu-item>
+            <el-menu-item index="3-1-2" @click="goToRoute('/games/how-do-i-even-win-this-game')">HDIEWTG?</el-menu-item>
+            <el-menu-item index="3-1-3" @click="goToRoute('/games/a-nice-place-4-art')">A nice play 4 art</el-menu-item>
+            <el-menu-item index="3-1-4" @click="goToRoute('/games/the-half-life-of-flerovium')">The half life of flerovium</el-menu-item>
+            <el-menu-item index="3-1-5" @click="goToRoute('/games/medium-expendedore')">Medium Expendedore</el-menu-item>
+            <el-menu-item index="3-1-6" @click="goToRoute('/games/kiwiq')">Kiwiq</el-menu-item>
+          </el-sub-menu>
+          <el-sub-menu index="3-2">
+            <template #title>{{ t('projects.talks.title') }}</template>
+            <el-menu-item index="3-2-1" @click="goToRoute('/talks/sustainable-platform-engineering')">Sustainable Platform Engineering?</el-menu-item>
+          </el-sub-menu>
+          <el-menu-item index="3-3" @click="goToRoute('/blog')">Blog</el-menu-item>
+        </el-sub-menu>
+        <el-sub-menu index="4">
+          <template #title><el-icon><StarFilled /></el-icon>{{ isMobile() ? '': 'Hobbies' }}</template>
+          <el-menu-item index="4-1" @click="goToRoute('/celeste')">{{ t('celeste.title') }}</el-menu-item>
+          <el-menu-item index="4-2" @click="goToRoute('/minecraft')">Minecraft </el-menu-item>
+          <el-menu-item index="4-3" @click="goToRoute('/art')">{{ t('art.title') }}</el-menu-item>
+        </el-sub-menu>
+        <el-sub-menu index="5">
+          <template #title><el-icon><Tools /></el-icon></template>
+          <!-- <el-menu-item index="5-1" @click="setTheme('light')">{{ t('config.theme.light-mode') }}</el-menu-item>
+          <el-menu-item index="5-1" @click="setTheme('dark')">{{ t('config.theme.dark-mode') }}</el-menu-item>
+          <el-menu-item index="5-1" @click="setTheme('dark-typical')">{{ 'Dark Typical' }}</el-menu-item> -->
+          <el-menu-item index="5-2" @click="changeLanguage('es')">{{ t('config.language.spanish') }}</el-menu-item>
+          <el-menu-item index="5-3" @click="changeLanguage('en')">{{ t('config.language.english') }}</el-menu-item>
+        </el-sub-menu>
+      </el-menu>
     </el-row>
         
   </body>
@@ -95,7 +95,7 @@ nav a.router-link-exact-active {
   color: var(--secondary-color);
 }
 .mobile{
-  width:98%;
+  width:100%;
 }
 .desktop {
 }
