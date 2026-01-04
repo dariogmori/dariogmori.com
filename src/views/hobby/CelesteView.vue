9e216f41381ff36503eac3411b95538a4c655e46
Dario G. Mori <dario@resiz.es>
2026-01-04 10:57:09 +0100

    feat: refactor celeste view

--- src/views/hobby/CelesteView.vue
+++ src/views/hobby/CelesteView.vue
@@ -1,38 +1,22 @@
 <template>
   <ViewLayout>
-    <!-- LEFT CONTAINER -->
-    <template #left-col>
-      <LayoutRow>
-        <img alt="celeste game" src="https://gaming-cdn.com/images/products/8003/orig/celeste-pc-mac-juego-steam-cover.jpg?v=1705489821" class="image-display" style="width: 90%">
-      </LayoutRow>
-
-      <LayoutRow>
-        <el-col :span="16">
-          <p>
-            {{ t('celeste.description') }}
-          </p>
-        </el-col>
-        <el-col :span="8">
-          <img alt="madeline" src="https://media.spelunky.fyi/mods/logo/01G8PXMPWZ2K3CVW70DJ5HEMGA/1658626661282822.gif" class="image-display" style="width: 90%">
-        </el-col>
-      </LayoutRow>
-    </template>
 
     <!-- CENTER CONTAINER -->
     <template #center-col>
       <img src="../../assets/celeste/logo.gif" class="logo">
-    </template>
-
-    <!-- RIGHT CONTAINER -->
-    <template #right-col>
-      <LayoutRow>
-        <CelesteComponent />
+      <!-- <el-divider></el-divider> -->
+      <LayoutRow :justify="'center'">
+        <CelesteComponent style="max-width: 80%;"/>
       </LayoutRow>
-      <LayoutRow>
-        <VideoCardComponent :video="'https://www.youtube.com/embed/_iBkkwy0fcM'" :title="'Cool speedy thingie'" :article="'yay me go fast'" ></VideoCardComponent>
+      <LayoutRow :justify="'center'">
+        <p style="max-width: 80%;">
+          {{ t('celeste.description') }}
+        </p>
+      </LayoutRow>
+      <LayoutRow :justify="'center'">
+        <VideoCardComponent :video="'https://www.youtube.com/embed/_iBkkwy0fcM'" :title="'Cool speedy thingie'" :article="'yay me go fast'" style="max-width: 80%;" ></VideoCardComponent>
       </LayoutRow>
     </template>
-
   </ViewLayout>
 </template>
 
