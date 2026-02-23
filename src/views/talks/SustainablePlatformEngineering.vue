2faeee05667094776908fe9f7450fbf0f11a115e
Dario G. Mori <dariogmori@gmail.com>
2026-02-24 00:16:07 +0100

    fix: renamed files to properly use png now

--- src/views/talks/SustainablePlatformEngineering.vue
+++ src/views/talks/SustainablePlatformEngineering.vue
@@ -10,19 +10,19 @@
                 <LayoutRow :justify="'end'">
                    <el-carousel indicator-position="outside" :interval="4000" height="33vh" style="width:100%;">
                        <el-carousel-item>
-                           <img :src="'../../img/projects/talks/sustainability/talk-title.jpg'" class="image-display" style="width: 100%">
+                           <img :src="'../../img/projects/talks/sustainability/talk-title.png'" class="image-display" style="width: 100%">
                        </el-carousel-item>
                        <el-carousel-item>
-                           <img :src="'../../img/projects/talks/sustainability/guadalajara-1.jpg'" class="image-display" style="width: 50%">
+                           <img :src="'../../img/projects/talks/sustainability/guadalajara-1.png'" class="image-display" style="width: 50%">
                        </el-carousel-item>
                        <el-carousel-item>
-                           <img :src="'../../img/projects/talks/sustainability/talk-1.jpg'" class="image-display" style="width: 100%">
+                           <img :src="'../../img/projects/talks/sustainability/talk-1.png'" class="image-display" style="width: 100%">
                        </el-carousel-item>
                    </el-carousel>
                 </LayoutRow>
                 <LayoutRow :justify="'center'">
                     <p> {{ t('projects.talks.kcd-guadalajara.thanks') }}</p>
-                    <img alt="Main Presentation" src="/img/projects/talks/sustainability/logo-kcd.jpg" class="image-display" style="width: 120px; ">
+                    <img alt="Main Presentation" src="/img/projects/talks/sustainability/logo-kcd.png" class="image-display" style="width: 120px; ">
                 </LayoutRow>
             </template>
 
@@ -55,7 +55,7 @@
                     <el-button class="cool-3d-text" @click="ad.show = false" style="border: 3px solid var(--main-color);">x</el-button>
                 </div>
                 <a href="https://www.youtube.com/watch?v=lTLttAcs1zE" target="_blank" rel="noopener noreferrer">
-                    <img alt="Main Presentation" src="/img/projects/talks/sustainability/promo-1.jpg" style="width: 100%; cursor: pointer;border: 3px solid var(--secondary-color);"/>
+                    <img alt="Main Presentation" src="/img/projects/talks/sustainability/promo-1.png" style="width: 100%; cursor: pointer;border: 3px solid var(--secondary-color);"/>
                 </a>
             </div>
             
