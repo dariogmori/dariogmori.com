412cd06315ea6190f5d18cc811b2575d266c6c2f
Dario G. Mori <dariogmori@gmail.com>
2026-03-18 21:03:11 +0100

    chore: using proper layout for talks page

--- src/views/talks/SustainablePlatformEngineering.vue
+++ src/views/talks/SustainablePlatformEngineering.vue
@@ -1,51 +1,42 @@
 <template>
-    <div>
+	<div>
         <ViewLayout>
-            <template #left-col>
-                <LayoutRow>
-                    <p>
-                        {{ t('projects.talks.kcd-guadalajara.description') }}
-                    </p>
+            <template #content>
+            	<h1> Sustainable Platform Engineering? </h1>
+                <h3>Having a green thumb without shooting up the costs</h3>
+                <LayoutRow :justify="'center'">
+                	<div style="width:48%; display: flex; justify-content:center;">
+	                    <p>>>></p>
+	                    <a href="https://www.youtube.com/embed/lTLttAcs1zE?si=EdE_M3TAI53or9WX" style="align-content: center; margin: 10px;"> {{ 'Youtube Video' }} </a>
+	                    <p>{{'<<<'}}</p>
+                    </div>
+                    <div style="width:48%; display: flex;  justify-content:center;">
+	                    <p>>>></p>
+	                    <a href="https://www.slideshare.net/slideshow/sustainable-platform-engineering-having-a-green-thumb-without-shooting-up-the-costs-dario-gutierrez-mori/277450234" style="align-content: center;  margin: 10px;"> {{ t('projects.talks.kcd-guadalajara.slides') }} </a>
+	                    <p>{{'<<<'}}</p>
+                    </div>
                 </LayoutRow>
-                <LayoutRow :justify="'end'">
-                   <el-carousel indicator-position="outside" :interval="4000" height="33vh" style="width:100%;">
-                       <el-carousel-item>
-                           <img :src="'../../img/projects/talks/sustainability/talk-title.png'" class="image-display" style="width: 100%">
-                       </el-carousel-item>
-                       <el-carousel-item>
-                           <img :src="'../../img/projects/talks/sustainability/guadalajara-1.png'" class="image-display" style="width: 50%">
-                       </el-carousel-item>
-                       <el-carousel-item>
-                           <img :src="'../../img/projects/talks/sustainability/talk-1.png'" class="image-display" style="width: 100%">
-                       </el-carousel-item>
-                   </el-carousel>
-                </LayoutRow>
+				<LayoutRow justify="space-around">
+	               	<img :src="'../../img/projects/talks/sustainability/talk-title.png'" class="image-display" style="width: 40%; height: 30%; rotate:10deg;">
+  	                <img :src="'../../img/projects/talks/sustainability/talk-1.png'" class="image-display" style="width: 40%; height: 30%; rotate: -10deg">
+				</LayoutRow>
+				<div style="justify-content:center; display: flex;" >
+	                <p style="width:80%;">
+	                	{{ t('projects.talks.kcd-guadalajara.description') }}
+	                </p>
+                </div>
+                
+				<LayoutRow justify="space-around">	  
+					<img :src="'../../img/projects/talks/sustainability/guadalajara-1.png'" class="image-display"  style="width: 40%; ">
+				</LayoutRow>
+                <div style="justify-content:center; display: flex;" >
+	                <p style="width:80%;"> {{ t('projects.talks.kcd-guadalajara.to_be_fair') }}</p>
+                </div>
+                <img alt="Main Presentation" src="/img/projects/talks/sustainability/logo-kcd.png" class="image-display" style="width: 120px; ">
+                                
                 <LayoutRow :justify="'center'">
                     <p> {{ t('projects.talks.kcd-guadalajara.thanks') }}</p>
-                    <img alt="Main Presentation" src="/img/projects/talks/sustainability/logo-kcd.png" class="image-display" style="width: 120px; ">
                 </LayoutRow>
-            </template>
-
-            <template #center-col>
-                <h1> Sustainable Platform Engineering? </h1>
-                <h3>Having a green thumb without shooting up the costs</h3>
-                </template>
-
-            <template #right-col>
-                <LayoutRow :justify="'center'">
-                    <p>>>></p>
-                    <a href="https://www.youtube.com/embed/lTLttAcs1zE?si=EdE_M3TAI53or9WX"> {{ 'Youtube Video' }} </a>
-                    <p>{{'<<<'}}</p>
-                </LayoutRow>
-                <LayoutRow :justify="'center'">
-                    <p>>>></p>
-                    <a href="https://www.slideshare.net/slideshow/sustainable-platform-engineering-having-a-green-thumb-without-shooting-up-the-costs-dario-gutierrez-mori/277450234"> {{ t('projects.talks.kcd-guadalajara.slides') }} </a>
-                    <p>{{'<<<'}}</p>
-                </LayoutRow>
-                <LayoutRow :justify="'center'">
-                    <p> {{ t('projects.talks.kcd-guadalajara.to_be_fair') }}</p>
-                </LayoutRow>
-            
             </template>
         </ViewLayout>
         <LayoutRow :justify="'end'">
@@ -119,5 +110,11 @@ import { reactive } from "vue";
         1px -1px 0 var(--secondary-color),
         -1px  1px 0 var(--secondary-color),
         1px  1px 0 var(--secondary-color);           
+  }
+  p {
+  	 font-family: 'Courier New', Courier, monospace;
+	 font-style: italic;
+	 font-size: 18px;
+	 text-align: left;
   }
   </style>
