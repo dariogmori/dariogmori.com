060883d421b690c5935406d059c0bdbd5f358bac
Dario G. Mori <dariogmori@gmail.com>
2026-03-18 20:17:31 +0100

    feat: remove element-plus from description about me mobile component

--- src/components/home/DescriptionAboutMeComponentMobile.vue
+++ src/components/home/DescriptionAboutMeComponentMobile.vue
@@ -1,49 +1,47 @@
 <template>
-  <el-row class="header-color" >
-    <el-col>
-      <h1> Darío Gutiérrez Mori </h1>
-      <h3> Mid Platform Engineer</h3>
-      <br>
-      <el-row :justify="'center'" >
-        <img src="/img/me.jpg" alt="Me" style="width: 70%; border-radius: 10px">
+  <div style="display: flex; width: 100%;">
+   	<div style="width:100%;">
+   	  <h1> Darío Gutiérrez Mori </h1>
+ 	  <h2> Mid Platform Engineer</h2>
+	  <el-row :justify="'center'" style="margin:20px" >
+         <img src="/img/me.jpg" alt="Me" style="width: 70%; border-radius: 10px">
       </el-row>
-      <br>
-      <el-row :justify="'space-between'">
-        <el-col :span="8">
-          <a href="https://dariogmori.itch.io/">
-            <el-row :justify="'center'">
-              <el-col :span="4">
-                <img src="../../assets/home/itchio.png" alt="itchio logo" class="image-display" style="image-rendering: crisp-edges;"/>
-              </el-col>
-            </el-row>
-          </a>
-        </el-col>
-        <el-col :span="8">
-          <a href="https://www.linkedin.com/in/dariogmori/">
-            <el-row :justify="'center'">
-              <el-col :span="4">
-                <img src="../../assets/home/linkedin.png" alt="itchio logo" class="image-display" style="image-rendering: crisp-edges;"/>
-              </el-col>
-            </el-row>
-          </a>
-        </el-col>
-        <el-col :span="8">
-          <a href="https://github.com/dariogmori">
-            <el-row :justify="'center'">
-              <el-col :span="4">
-                <img src="../../assets/home/github.png" alt="itchio logo" class="image-display" style="image-rendering: crisp-edges;"/>
-              </el-col>
-            </el-row>
-          </a>
-        </el-col>
-      </el-row>
-      <el-row :justify="'center'">
-        <el-col :span="20">
-          <p>{{ t('home.about-me')}}</p>
-        </el-col>
-      </el-row>
-    </el-col>
-  </el-row>
+ 	  <div style="justify-content:space-between; display: flex;">
+          <div style="width:33%;">
+            <a href="https://dariogmori.itch.io/">
+              <div style="display: flex; justify-content: center;">
+                <div>
+                  <img src="../../assets/home/itchio.png" alt="itchio logo" class="image-display" style="image-rendering: crisp-edges;"/>
+                </div>
+              </div>
+            </a>
+          </div>
+         <div style="width:33%;">
+            <a href="https://www.linkedin.com/in/dariogmori/">
+              <div style="display: flex; justify-content: center;">
+                <div>
+                  <img src="../../assets/home/linkedin.png" alt="itchio logo" class="image-display" style="image-rendering: crisp-edges;"/>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div style="width:33%;">
+            <a href="https://github.com/dariogmori">
+              <div style="display: flex; justify-content: center;">
+                <div>
+                  <img src="../../assets/home/github.png" alt="itchio logo" class="image-display" style="image-rendering: crisp-edges;"/>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+        <el-row :justify="'center'">
+      	 <el-col :span="20">
+     		 <p>{{ t('home.about-me')}}</p>
+          </el-col>
+       </el-row>
+   	</div>
+  </div>
 </template>
 <script setup lang="ts">
 import {useTranslation} from "i18next-vue";
